adffd96c2ce14394da4316e7676adb05e09165b0
Vikash Sharma <vs8824394430@gmail.com>
2024-10-15 22:31:31 +0530

    added search box

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,20 +1,23 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import './main.css'
 import{assets} from '../../assets/assets'
+import { Context } from '../../context/Context'
 const Main = () => {
+
+    const{onSent,recentPrompt,showResult,loading,resultData,setInput,input}=useContext(Context);
   return (
     <div className='main'>
         <div className="nav">
             <p>Gemini</p>
             <img src={assets.user_icon} alt="" />
         </div>
-        <div className="min-container">
-            <div className="greet">
+        <div className="main-container">
+            {!showResult?<>
+                <div className="greet">
             <p><span>Hello, Dev.</span></p>
             <p >How can i help you today?</p>
             </div>
-        </div>
-        <div className="cards">
+            <div className="cards">
             <div className="card">
                 <p>Suggest beautiful places to see on an upcoming road trip</p>
                 <img src={assets.compass_icon} alt="" />
@@ -32,22 +35,38 @@ const Main = () => {
                 <img src={assets.code_icon} alt="" />
             </div>
         </div>
+            </>:<div className="result">
+                 <div className="result-tittle">
+                    <img src={assets.user_icon} alt="" />
+                    <p>{recentPrompt}</p>
+                 </div>
+                 <div className="result-data">
+                    <img src={assets.gemini_icon} alt="" />
+                    {loading?<div className="loader">
+                        <hr />
+                        <hr />
+                        <hr />
+                    </div>:
+                    <p dangerouslySetInnerHTML={{__html:resultData}}></p>}
+                 </div>
+                </div>
+                }
+            
         <div className="main-bottom">
             <div className="search-box">
-                <input type="text" placeholder="enter a promt here" />
+                <input onChange={(e)=>setInput(e.target.value)} value={input} type="text" placeholder="enter a promt here" />
                 <div>
                     <img src={assets.gallery_icon} alt="" />
                     <img src={assets.mic_icon} alt="" />
-                    <img src={assets.send_icon} alt="" />
+                    <img onClick={()=>onSent()}src={assets.send_icon} alt="" />
                 </div>
             </div>
             <p className="bottom-info">
                 Gemini may display inaccurate info, including about people, so double check it's responses. Your privacy and Gemini Apps.
-                
             </p>
         </div>
-      
-    </div>
+        </div>
+        </div>
   )
 }
 
--- src/context/Context.jsx
+++ src/context/Context.jsx
@@ -1,15 +1,40 @@
-import { createContext } from "react";
+import { createContext, useState } from "react";
 import run from '../config/gemini'
 export const Context=createContext();
 
 const ContextProvider=(props)=>{
+
+    const [input,setInput]=useState("");
+    const [recentPrompt,setRecentPrompt]=useState("");
+    const [prevPrompts,setPrevPrompts]=useState([]);
+    const [showResult,setShowResult]=useState(false);
+    const [loading,setLoading]=useState(false);
+    const [resultData,setResultData]=useState("");
+    
     const onSent=async(prompt)=>{
-       await run(prompt)
+        setResultData("")
+        setLoading(true)
+        setShowResult(true)
+        setRecentPrompt(input)
+        const response=await run(input);
+        setResultData(response);
+        setLoading(false);
+        setInput("");
+
     }
 
-    onSent("what is react.js?");
+    // onSent("what is react.js?");
     const contextValue={
-
+         prevPrompts,
+         setPrevPrompts,
+         onSent,
+         setRecentPrompt,
+         recentPrompt,
+         showResult,
+         loading,
+         resultData,
+         input,
+         setInput
     }
     return (
         <Context.Provider value={contextValue}>
