a5553fbbf70e98644bb8c45c1a32e2fc3a768265
Vikash Sharma <vs8824394430@gmail.com>
2024-11-01 16:32:47 +0530

    new search

--- src/context/Context.jsx
+++ src/context/Context.jsx
@@ -16,6 +16,11 @@ const ContextProvider=(props)=>{
                   setResultData(prev=>prev+nextWord);
              },75*index)
     }
+    const newChat=()=>{
+        setLoading(false)
+        setShowResult(false)
+
+    }
     const onSent=async(prompt)=>{
         setResultData("")
         setLoading(true)
@@ -64,7 +69,8 @@ const ContextProvider=(props)=>{
          loading,
          resultData,
          input,
-         setInput
+         setInput,
+         newChat
     }
     return (
         <Context.Provider value={contextValue}>
